b9c0e698b5e0cc228fc611aff3ee0f6487302cc0
Mikhail Astashkevich <asmisha@tut.by>
2022-07-29 18:15:03 +0200

    fix

--- gainy/utils.py
+++ gainy/utils.py
@@ -4,7 +4,10 @@ import psycopg2
 from psycopg2._psycopg import connection
 import numpy as np
 import logging
+import sys
 from pythonjsonlogger import jsonlogger
+import datetime
+import traceback
 
 
 def batch_iter(ary, batch_size: int = 100) -> Iterable[List[Any]]:
@@ -45,14 +48,54 @@ def db_connect() -> connection:
     return psycopg2.connect(DB_CONN_STRING)
 
 
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record,
+                                                    message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+
+        log_record['pathname'] = record.pathname
+        log_record['lineno'] = record.lineno
+
+
 def get_logger(name):
     logging.basicConfig()
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG if env() == "local" else logging.INFO)
 
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
     logHandler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter()
-    logHandler.setFormatter(formatter)
     logger.addHandler(logHandler)
+    formatter = CustomJsonFormatter()
+    logHandler.setFormatter(formatter)
 
     return logger
+
+
+sys._excepthook = sys.excepthook  # save original excepthook
+
+
+def exception_hook(exctype, value, tb):
+    trace = [{
+        "filename": frame.filename,
+        "lineno": frame.lineno,
+        "name": frame.name
+    } for frame in traceback.extract_tb(tb)]
+
+    get_logger("root").exception(value,
+                                 extra={
+                                     "exc_type": exctype.__name__,
+                                     "traceback": trace
+                                 })
+
+
+sys.excepthook = exception_hook  # overwrite default excepthook
