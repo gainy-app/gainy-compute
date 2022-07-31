3d94f59dd558bb5d5d0236d7d40780d990d40053
Mikhail Astashkevich <asmisha@tut.by>
2022-07-31 12:44:48 +0200

    fix

--- gainy/utils.py
+++ gainy/utils.py
@@ -10,6 +10,32 @@ import datetime
 import traceback
 
 
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record,
+                                                    message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+
+        log_record['pathname'] = record.pathname
+        log_record['lineno'] = record.lineno
+
+
+formatter = CustomJsonFormatter()
+
+LOG_LEVEL = logging.DEBUG if env() == "local" else logging.INFO
+LOG_HANDLER = logging.StreamHandler()
+LOG_HANDLER.setFormatter(formatter)
+logging.basicConfig(level=LOG_LEVEL, handlers=[LOG_HANDLER])
+
+
 def batch_iter(ary, batch_size: int = 100) -> Iterable[List[Any]]:
     n_chunks = trunc(len(ary) / batch_size) + 1
     return np.array_split(ary, n_chunks)
@@ -48,35 +74,14 @@ def db_connect() -> connection:
     return psycopg2.connect(DB_CONN_STRING)
 
 
-class CustomJsonFormatter(jsonlogger.JsonFormatter):
-
-    def add_fields(self, log_record, record, message_dict):
-        super(CustomJsonFormatter, self).add_fields(log_record, record,
-                                                    message_dict)
-        if not log_record.get('timestamp'):
-            # this doesn't use record.created, so it is slightly off
-            now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-            log_record['timestamp'] = now
-        if log_record.get('level'):
-            log_record['level'] = log_record['level'].upper()
-        else:
-            log_record['level'] = record.levelname
-
-        log_record['pathname'] = record.pathname
-        log_record['lineno'] = record.lineno
-
-
 def get_logger(name):
-    logging.basicConfig()
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG if env() == "local" else logging.INFO)
+    logger.setLevel(LOG_LEVEL)
 
-    formatter = CustomJsonFormatter()
     for handler in logger.handlers:
-#         logger.removeHandler(handler)
-#     logHandler = logging.StreamHandler()
-#     logger.addHandler(logHandler)
-        handler.setFormatter(formatter)
+        logger.removeHandler(handler)
+
+    logger.addHandler(LOG_HANDLER)
 
     return logger
 
