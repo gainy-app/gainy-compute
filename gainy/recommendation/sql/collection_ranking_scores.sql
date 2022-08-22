d991dbf62aec37b86ecde2a064dec73f39b8f8c0
Mikhail Astashkevich <asmisha@tut.by>
2022-08-22 14:29:16 +0200

    Refactor storing collection match scores

--- gainy/recommendation/sql/collection_ranking_scores.sql
+++ gainy/recommendation/sql/collection_ranking_scores.sql
@@ -1,9 +1,9 @@
-select c.id as collection_id, coalesce(collection_match_score.match_score, 0.0) as ranking_score
+select c.id as collection_id, coalesce(profile_collection_match_score.match_score, 0.0) as ranking_score
 from profile_collections c
-         left join collection_match_score
-                   on c.id = collection_match_score.collection_id
-where collection_match_score.profile_id = %(profile_id)s
+         left join app.profile_collection_match_score
+                   on c.uniq_id = profile_collection_match_score.collection_uniq_id
+where profile_collection_match_score.profile_id = %(profile_id)s
   and c.enabled = '1'
   and c.personalized = '0'
-order by collection_match_score.interest_level desc, collection_match_score.match_score desc nulls last
+order by profile_collection_match_score.interest_level desc, profile_collection_match_score.match_score desc nulls last
 limit %(limit)s;
