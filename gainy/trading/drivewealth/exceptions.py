1593ce79aa07f677ad8615627a74d7073044b932
Mikhail Astashkevich <asmisha@tut.by>
2022-10-06 17:54:12 +0200

    Api error handling

---
+++ gainy/trading/drivewealth/exceptions.py
@@ -0,0 +1,30 @@
+from gainy.exceptions import ApiException
+
+
+class DriveWealthApiException(ApiException):
+
+    def __init__(self, status_code, code, message):
+        self.status_code = status_code
+        self.code = code
+        self.message = "%s: %s" % (code, message)
+
+    @staticmethod
+    def create_from_response(data, status_code):
+        error_code_to_exception_class = {"I050": InstrumentNotFoundException}
+
+        message = "Failed: %d" % status_code
+        error_code = None
+        if data:
+            message = data.get("message", message)
+            error_code = data.get("errorCode")
+
+        if error_code in error_code_to_exception_class:
+            cls = error_code_to_exception_class[error_code]
+        else:
+            cls = DriveWealthApiException
+
+        return cls(status_code, error_code, message)
+
+
+class InstrumentNotFoundException(DriveWealthApiException):
+    pass
