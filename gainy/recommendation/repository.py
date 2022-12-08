85efbbcf7762d54124153ac5d196916cf4428834
Mikhail Astashkevich <asmisha@tut.by>
2022-12-08 09:42:40 +0100

    fix

--- gainy/recommendation/repository.py
+++ gainy/recommendation/repository.py
@@ -25,7 +25,7 @@ class RecommendedCollectionAlgorithm(enum.Enum):
 
 def _read_sorted_collection_manually_selected(limit: int) -> List[int]:
     data = RECOMMENDATION_MANUALLY_SELECTED_COLLECTION_IDS
-    data = [int(i) for i in data]
+    data = [int(i) for i in data if i]
     return data[:limit]
 
 
