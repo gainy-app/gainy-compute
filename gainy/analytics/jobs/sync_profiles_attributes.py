d78577d8f93d933b2d55c2b9d8afa7238c6bc04a
Mikhail Astashkevich <asmisha@tut.by>
2023-02-16 14:54:33 +0100

    fix

--- gainy/analytics/jobs/sync_profiles_attributes.py
+++ gainy/analytics/jobs/sync_profiles_attributes.py
@@ -1,32 +1,47 @@
+from typing import Iterable
+
 import argparse
 import traceback
 
 from gainy.context_container import ContextContainer
+from gainy.utils import get_logger, env, ENV_PRODUCTION
+
+logger = get_logger(__name__)
+
+
+def iterate_profile_ids(db_conn) -> Iterable[int]:
+    with db_conn.cursor() as cursor:
+        query = "SELECT id FROM app.profiles"
+        if env() == ENV_PRODUCTION:
+            query += " where email not ilike '%test%@gainy.app'"
+
+        cursor.execute(query)
+        for row in cursor:
+            yield row[0]
 
 
 def cli(args=None):
     parser = argparse.ArgumentParser(
         description='Update profiles\' analytics attributes.')
-    parser.add_argument('--profile_id', type=int)
-    parser.add_argument('--batch_size', type=int, default=100)
+    parser.add_argument('--profile-id', dest='profile_id', type=int)
     args = parser.parse_args(args)
 
     profile_id = args.profile_id
-    batch_size = args.batch_size
 
     try:
         with ContextContainer() as context_container:
             if profile_id:
                 context_container.analytics_service.sync_profile_attribution(
                     profile_id)
+                logger.info(
+                    f'Profile attributes synced for profile {profile_id}')
                 return
 
-            repo = context_container.get_repository()
-            for profile_ids_batch in repo.read_batch_profile_ids(batch_size):
-                for profile_id in profile_ids_batch:
-                    context_container.analytics_service.sync_profile_attribution(
-                        profile_id)
-
+            for profile_id in iterate_profile_ids(context_container.db_conn):
+                context_container.analytics_service.sync_profile_attribution(
+                    profile_id)
+                logger.info(
+                    f'Profile attributes synced for profile {profile_id}')
     except Exception as e:
         traceback.print_exc()
         raise e
