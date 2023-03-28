3b6192fd9ee5ae2406d1498ecfaa7b9005756113
Mikhail Astashkevich <asmisha@tut.by>
2023-03-28 09:23:05 +0200

    calculate daily fees instead of monthly avg value based fee

--- gainy/billing/repository.py
+++ gainy/billing/repository.py
@@ -2,20 +2,12 @@ import os
 
 import datetime
 import dateutil.parser
-from decimal import Decimal
 from typing import Iterable, Optional
 
 from gainy.billing.exceptions import NoActivePaymentMethodException
 from gainy.billing.models import Invoice, InvoiceStatus, PaymentMethod, PaymentTransaction, TransactionStatus
 from gainy.data_access.operators import OperatorLt
 from gainy.data_access.repository import Repository
-
-# all yearly
-BILLING_VALUE_FEE_MULTIPLIER = os.getenv("BILLING_VALUE_FEE_MULTIPLIER", 0.01)
-BILLING_VALUE_FEE_MULTIPLIER = Decimal(BILLING_VALUE_FEE_MULTIPLIER)
-
-BILLING_MIN_YEARLY_FEE = os.getenv("BILLING_MIN_YEARLY_FEE", 100)
-BILLING_MIN_YEARLY_FEE = Decimal(BILLING_MIN_YEARLY_FEE)
 
 BILLING_ENABLED_PROFILES = os.getenv("BILLING_ENABLED_PROFILES")
 BILLING_ENABLED_PROFILES = BILLING_ENABLED_PROFILES.split(
@@ -30,10 +22,7 @@ class BillingRepository(Repository):
 
     def create_invoices(self):
         conditions = ""
-        params = {
-            "min_fee": BILLING_MIN_YEARLY_FEE,
-            "value_fee_multiplier": BILLING_VALUE_FEE_MULTIPLIER,
-        }
+        params = {}
 
         if BILLING_MIN_DATE:
             params["min_period_start"] = BILLING_MIN_DATE
@@ -48,29 +37,26 @@ class BillingRepository(Repository):
                 f"""insert into app.invoices(profile_id, period_id, amount, due_date, description, period_start, period_end, metadata)
                     with drivewealth_monthly_usage_extended as
                              (
-                                 select *,
-                                        'mo_' || period_start::date::varchar as period_id
-                                 from drivewealth_monthly_usage
+                                 select profile_id,
+                                        period_start,
+                                        period_end,
+                                        'mo_' || period_start::date::varchar as period_id,
+                                        sum(fee) as fee
+                                 from drivewealth_daily_fees
                                  where profile_id is not null
-                                   and value > 0
                                    {conditions}
+                                 group by profile_id, period_start, period_end
+                                 having sum(fee) > 0
                          )
                     select drivewealth_monthly_usage_extended.profile_id,
                            drivewealth_monthly_usage_extended.period_id,
-                           greatest(%(min_fee)s, value * %(value_fee_multiplier)s)::numeric *
-                           -- days in month
-                           extract(days from
-                                   drivewealth_monthly_usage_extended.period_end - drivewealth_monthly_usage_extended.period_start) /
-                           -- days in year
-                           extract(days from
-                                   date_trunc('year', drivewealth_monthly_usage_extended.period_start) + interval '1 year' -
-                                   date_trunc('year', drivewealth_monthly_usage_extended.period_start)) as amount,
-                           drivewealth_monthly_usage_extended.period_end::date + interval '1 day'       as due_date,
+                           fee                                                                    as amount,
+                           drivewealth_monthly_usage_extended.period_end::date + interval '1 day' as due_date,
                            'Invoice for Gainy services in ' ||
-                           TO_CHAR(drivewealth_monthly_usage_extended.period_start, 'Month')            as description,
+                           TO_CHAR(drivewealth_monthly_usage_extended.period_start, 'Month')      as description,
                            drivewealth_monthly_usage_extended.period_start,
                            drivewealth_monthly_usage_extended.period_end,
-                           null                                                                         as metadata
+                           null                                                                   as metadata
                     from drivewealth_monthly_usage_extended
                              left join app.invoices using (profile_id, period_id)
                     where drivewealth_monthly_usage_extended.period_end < now()
