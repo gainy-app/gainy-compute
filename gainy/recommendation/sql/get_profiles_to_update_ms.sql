186e8fea4e0f12a7e0c5037340c735eda0cc3c4b
Mikhail Astashkevich <asmisha@tut.by>
2023-06-26 09:26:01 +0200

    don't calculate MS for test users

--- gainy/recommendation/sql/get_profiles_to_update_ms.sql
+++ gainy/recommendation/sql/get_profiles_to_update_ms.sql
@@ -3,5 +3,8 @@ from profile_actual_recommendation_state
          left join app.object_recommendation_state
                    on object_recommendation_state.object_id = profile_id::text
                        and object_recommendation_state.object_type = 'profile'
-where profile_actual_recommendation_state.state_hash != object_recommendation_state.state_hash
-   or object_recommendation_state.state_hash is null
+         join app.profiles
+              on profiles.id = profile_id
+where email not ilike '%test%@gainy.app'
+  and (profile_actual_recommendation_state.state_hash != object_recommendation_state.state_hash
+    or object_recommendation_state.state_hash is null)
